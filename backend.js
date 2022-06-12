1d204651ebae8594380371b77260383452a08b3e
Mazbaur Rashid Meraz <mazbaurrashidmeraz@gmail.com>
2022-06-13 01:27:00 +0600

    js

--- backend.js
+++ backend.js
@@ -1,4 +1,51 @@
-function myFunction() {
-    var x = document.getElementById("myBtn");
-    x.disabled = true;
+var screen = document.getElementById("disp");
+var value = screen.value;
+function one() {
+    value = value + "1";
+    console.log(value);
+    screen.value = value;
+}
+function two() {
+    value = value + "2";
+    console.log(value);
+    screen.value = value;
+}
+function three() {
+    value = value + "3";
+    console.log(value);
+    screen.value = value;
+}
+function four() {
+    value = value + "4";
+    console.log(value);
+    screen.value = value;
+}
+function five() {
+    value = value + "5";
+    console.log(value);
+    screen.value = value;
+}
+function six() {
+    value = value + "6";
+    console.log(value);
+    screen.value = value;
+}
+function seven() {
+    value = value + "7";
+    console.log(value);
+    screen.value = value;
+}
+
+function eight() {
+    value = value + "8";
+    console.log(value);
+    screen.value = value;
+} function nine() {
+    value = value + "9";
+    console.log(value);
+    screen.value = value;
+} function zero() {
+    value = value + "0";
+    console.log(value);
+    screen.value = value;
 }
