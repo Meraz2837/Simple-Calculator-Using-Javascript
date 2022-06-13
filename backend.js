f39b9f5969d478e5bd73d7ff510914e88a555b6a
Mazbaur Rashid Meraz <mazbaurrashidmeraz@gmail.com>
2022-06-13 17:05:24 +0600

    color

--- backend.js
+++ backend.js
@@ -2,7 +2,7 @@ var numb1, numb2, result;
 var operator = "";
 operator = "bal"
 
-
+var body = document.getElementById("body")
 
 
 var screen = document.getElementById("disp");
@@ -11,56 +11,82 @@ function one() {
     value = value + "1";
     console.log(value);
     screen.value = value;
+    body.style.background = "black"
+    screen.style.color = "white"
 }
 function two() {
     value = value + "2";
     console.log(value);
     screen.value = value;
+    body.style.background = "yellow"
+    screen.style.color = "black"
 }
 function three() {
     value = value + "3";
     console.log(value);
     screen.value = value;
+    body.style.background = "tomato"
+    screen.style.color = "black"
 }
 function four() {
     value = value + "4";
     console.log(value);
     screen.value = value;
+    body.style.background = "antiquewhite"
+    screen.style.color = "black"
 }
 function five() {
     value = value + "5";
     console.log(value);
     screen.value = value;
+    body.style.background = "aqua"
+    screen.style.color = "black"
 }
 function six() {
     value = value + "6";
     console.log(value);
     screen.value = value;
+    body.style.background = "blueviolet"
+    screen.style.color = "white"
 }
 function seven() {
     value = value + "7";
     console.log(value);
     screen.value = value;
+    body.style.background = "brown"
+    screen.style.color = "black"
 }
 
 function eight() {
     value = value + "8";
     console.log(value);
     screen.value = value;
-} function nine() {
+    body.style.background = "chocolate"
+    screen.style.color = "white"
+}
+
+function nine() {
     value = value + "9";
     console.log(value);
     screen.value = value;
-} function zero() {
+    body.style.background = "crimson"
+    screen.style.color = "white"
+}
+
+function zero() {
     value = value + "0";
     console.log(value);
     screen.value = value;
+    body.style.background = "darkcyan"
+    screen.style.color = "white"
 }
 
 function C() {
     value = "";
     console.log(value);
     screen.value = value;
+    body.style.background = "white"
+    screen.style.color = "black"
 }
 
 
@@ -74,18 +100,32 @@ function plus() {
     numb1 = screen.value;
     value = "";
     screen.value = "";
+    body.style.background = "darkmagenta"
+    screen.style.color = "black"
 }
 function minus() {
     operator = "-";
-    numb1 = value;
+    numb1 = screen.value;
+    value = "";
+    screen.value = "";
+    body.style.background = "darkorange"
+    screen.style.color = "white"
 }
 function mul() {
     operator = "X";
-    numb1 = value;
+    numb1 = screen.value;
+    value = "";
+    screen.value = "";
+    body.style.background = "darkred"
+    screen.style.color = "white"
 }
 function division() {
     operator = "/";
-    numb1 = value;
+    numb1 = screen.value;
+    value = "";
+    screen.value = "";
+    body.style.background = "darkslategray"
+    screen.style.color = "white"
 }
 
 function calc() {
@@ -94,5 +134,19 @@ function calc() {
     if (operator === "+") {
         result = numb1 + numb2;
     }
+    else if (operator === "-") {
+        result = numb1 - numb2;
+    }
+
+    else if (operator === "X") {
+        result = numb1 * numb2;
+    }
+
+    else {
+        result = numb1 / numb2;
+    }
+
     screen.value = result
+    body.style.background = "indigo"
+    screen.style.color = "white"
 }
